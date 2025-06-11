8696c4714dda87aaa90301e04630abb679945455
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-11 14:33:39 +0800

    Update button_manager.py

--- scenes/button_manager.py
+++ scenes/button_manager.py
@@ -6,11 +6,9 @@ class ButtonManager:
         self.game = game
         self.screen = game.screen
         
-        # Load button images
-        self.play_again_image = pygame.image.load(os.path.join("assets", "images", "play_again.png"))
-        self.to_menu_image = pygame.image.load(os.path.join("assets", "images", "to_menu.png"))
-        
-        # Define button rects
+        # 更新：使用 load_image 載入按鈕圖片，統一 100x50
+        self.play_again_image = load_image(os.path.join("assets", "images", "play_again.png"), size=(100, 50))
+        self.to_menu_image = load_image(os.path.join("assets", "images", "to_menu.png"), size=(100, 50))
         self.play_again_rect = self.play_again_image.get_rect(topleft=(150, 600))
         self.to_menu_rect = self.to_menu_image.get_rect(topleft=(350, 600))
 
