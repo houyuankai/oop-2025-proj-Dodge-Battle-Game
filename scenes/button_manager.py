78dd75551af20259de4bb0da63ebde73e4b41f28
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-11 19:39:35 +0800

    Update button_manager.py

--- scenes/button_manager.py
+++ scenes/button_manager.py
@@ -1,13 +1,11 @@
 import pygame
 import os
-from scenes.utils import load_image  # 新增：匯入 load_image
+from scenes.utils import load_image
 
 class ButtonManager:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
-        
-        # 更新：使用 load_image 載入按鈕圖片，統一 100x50
         self.play_again_image = load_image(os.path.join("assets", "images", "play_again.png"), size=(100, 50))
         self.to_menu_image = load_image(os.path.join("assets", "images", "to_menu.png"), size=(100, 50))
         self.play_again_rect = self.play_again_image.get_rect(topleft=(150, 600))
@@ -17,11 +15,8 @@ class ButtonManager:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
             if self.play_again_rect.collidepoint(mouse_pos):
-                print("Play Again button clicked") 
                 battle_scene.reset_game()
-            
             elif self.to_menu_rect.collidepoint(mouse_pos):
-                print("Menu button clicked")  # 除錯
                 self.game.change_scene("menu")
 
     def draw(self, screen):
