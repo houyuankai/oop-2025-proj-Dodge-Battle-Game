027eb70f67e1671d865c071fc43a89e5ad8630c4
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-14 20:22:52 +0800

    Update item.py 大小＆機率＆秒數

--- scenes/item.py
+++ scenes/item.py
@@ -4,8 +4,8 @@ from scenes.utils import load_image
 
 class Item:
     def __init__(self, x, y, item_type):
-        self.rect = pygame.Rect(x, y, 20, 20)
+        self.rect = pygame.Rect(x, y, 40, 40)
         self.item_type = item_type  # "item1", "item2", "item3", "item4"
-        self.image = load_image(os.path.join("assets", "images", f"{item_type}.png"), size=(20, 20))
+        self.image = load_image(os.path.join("assets", "images", f"{item_type}.png"), size=(40, 40))
         self.spawn_time = pygame.time.get_ticks()
-        self.lifetime = 2500  # 2.5 秒
+        self.lifetime = 3000  # 3 秒
