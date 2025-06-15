f26b8a57dd632a6650d209a388a03dece3eb5cd3
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-15 14:47:48 +0800

    Update projectile.py

--- scenes/projectile.py
+++ scenes/projectile.py
@@ -8,12 +8,12 @@ class Projectile:
         self.angle = angle
       
         if vx == 0:
-            self.surface = pygame.Surface((200 * rect.width/200, 20 * rect.height/20), pygame.SRCALPHA)
+            self.surface = pygame.Surface((200, 20), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
         elif vy == 0:
-            self.surface = pygame.Surface((20 * rect.width/20, 200 * rect.height/200), pygame.SRCALPHA)
+            self.surface = pygame.Surface((20, 200), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
         else:
-            self.surface = pygame.Surface((20 * rect.width/20, 200 * rect.height/200), pygame.SRCALPHA)
+            self.surface = pygame.Surface((20, 200), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
             self.surface = pygame.transform.rotate(self.surface, angle)
