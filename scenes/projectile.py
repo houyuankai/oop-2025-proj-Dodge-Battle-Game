e1d107fa2a0fc1c5792fcba30dcbb03ac6dfd381
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-15 14:29:05 +0800

    Update projectile.py

--- scenes/projectile.py
+++ scenes/projectile.py
@@ -5,16 +5,15 @@ class Projectile:
         self.rect = rect
         self.vx = vx
         self.vy = vy
-        self.angle = angle  # 旋轉角度
+        self.angle = angle
       
-        # 根據方向和尺寸創建表面
-        if vx == 0:  # 上下移動
-            self.surface = pygame.Surface((200, 20), pygame.SRCALPHA)
+        if vx == 0:
+            self.surface = pygame.Surface((200 * rect.width/200, 20 * rect.height/20), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
-        elif vy == 0:  # 左右移動
-            self.surface = pygame.Surface((20, 200), pygame.SRCALPHA)
+        elif vy == 0:
+            self.surface = pygame.Surface((20 * rect.width/20, 200 * rect.height/200), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
-        else:  # 斜向移動
-            self.surface = pygame.Surface((20, 200), pygame.SRCALPHA)
+        else:
+            self.surface = pygame.Surface((20 * rect.width/20, 200 * rect.height/200), pygame.SRCALPHA)
             self.surface.fill((255, 255, 255))
             self.surface = pygame.transform.rotate(self.surface, angle)
