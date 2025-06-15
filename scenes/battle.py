b3ccb4cd20a75bd0c9fd896ecdcc773ed8090c4d
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-15 13:04:33 +0800

    Update battle.py

--- scenes/battle.py
+++ scenes/battle.py
@@ -342,7 +342,7 @@ class BattleScene:
                     while len(self.items) > 0 and math.hypot(x2 - x1, y2 - y1) < 50:
                         x2 = random.randint(50, 550)
                         y2 = random.randint(350, 850)
-                    if r2 < 0.7:  # 7% key1
+                    if r2 < 0.07:  # 7% key1
                         self.items.append(Item(x2, y2, "key1"))
                         print(f"Spawned key1 at ({x2}, {y2}), boss_hp: {self.boss_hp}")
                     elif r2 < 0.14:  # 7% key2
