66bc343aeb1ed787fa432630e608e6be86f795bb
houyuankai <houyuankai431.md13@nycu.edu.tw>
2025-06-15 14:47:33 +0800

    Update menu.py

--- scenes/menu.py
+++ scenes/menu.py
@@ -9,12 +9,12 @@ class MenuScene:
         self.screen = game.screen
         self.scale = game.scale
         
-        self.background = load_image(os.path.join("assets", "images", "menu_background.png"), size=(600 * self.scale, 900 * self.scale))
-        self.start_button = load_image(os.path.join("assets", "images", "start_button.png"), size=(150 * self.scale, 75 * self.scale))
-        self.start_button_rect = self.start_button.get_rect(center=(300 * self.scale, 750 * self.scale))
-        self.title_font = pygame.font.SysFont("Arial", int(64 * self.scale))
+        self.background = load_image(os.path.join("assets", "images", "menu_background.png"), size=(600, 900))
+        self.start_button = load_image(os.path.join("assets", "images", "start_button.png"), size=(150, 75))
+        self.start_button_rect = self.start_button.get_rect(center=(300, 750))
+        self.title_font = pygame.font.SysFont("Arial", 64)
         self.title_surface = self.title_font.render("Dungeon Boss Fight", True, (255, 255, 255))
-        self.title_rect = self.title_surface.get_rect(center=(300 * self.scale, 200 * self.scale))
+        self.title_rect = self.title_surface.get_rect(center=(300, 200))
         self.music_path = os.path.join("assets", "sounds", "music_1.mp3")
 
     def handle_event(self, event):
